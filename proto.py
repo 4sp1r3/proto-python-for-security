edfec03f18bacbef125ad2c3ce67abf7eae2f818
chrisduffy <christopher.s.duffy@gmail.com>
2015-07-15 15:37:16 -0400

    Update proto.py

--- proto.py
+++ proto.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
-# Author: Chris Duffy
-# Date: June 4, 2014
-# Credit: Shawn Evans for netcider.py, which the cidr class was extracted and slightly modified
-# Email: Shawn.Evans@KnowledgeCG.com
 '''
- Purpose: This script is designed to provide teams the means to quickly generate useful code for assessments.
+Author: Chris Duffy
+Date: June 4, 2014
+Credit: Shawn Evans for netcider.py, which the cidr class was extracted and slightly modified
+Email: Shawn.Evans@KnowledgeCG.com
+Purpose: This script is designed to provide teams the means to quickly generate useful code for assessments.
  All the specific nuances of IP imports, file parsing and so forth have been handled so that an tester can just
  generate the needed code.
+
+Copyright (c) 2015, Christopher Duffy All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met: * Redistributions 
+of source code must retain the above copyright notice, this list of conditions and 
+the following disclaimer. * Redistributions in binary form must reproduce the above 
+copyright notice, this list of conditions and the following disclaimer in the 
+documentation and/or other materials provided with the distribution. * Neither the 
+name of the nor the names of its contributors may be used to endorse or promote 
+products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL CHRISTOPHER DUFFY BE LIABLE FOR ANY DIRECT, INDIRECT, 
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
-# Disclaimer: This tool should be not be modified or used with malicious intentions
-# Some of these libraries are not needed, they were included based on common requirements
+
 import sys
 import operator
 import itertools
